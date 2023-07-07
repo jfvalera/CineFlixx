26f4bf8c54d217960fa37257fbadef7d33c79998
Juan.apk <105395517+jfvalera@users.noreply.github.com>
2023-07-07 10:01:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var movieGrid = document.getElementById('content');
-  
-    // Cargar el archivo JSON
-    fetch('peliculas.json')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        var movies = data.peliculas;
-  
-        // Renderizar las películas en la página
-        movies.forEach(function(movie) {
-          var movieItem = document.createElement('div');
-          movieItem.className = 'movie-item';
-  
-          var movieImage = document.createElement('img');
-          movieImage.src = movie.imagen;
-  
-          var movieOverlay = document.createElement('div');
-          movieOverlay.className = 'movie-overlay';
-  
-          var movieTitle = document.createElement('h3');
-          movieTitle.textContent = movie.titulo;
-  
-          var watchLink = document.createElement('a');
-          watchLink.className = 'btn-watch';
-          watchLink.href = movie.enlace;
-          watchLink.textContent = 'Ver ahora';
-  
-          movieOverlay.appendChild(movieTitle);
-          movieOverlay.appendChild(watchLink);
-  
-          movieItem.appendChild(movieImage);
-          movieItem.appendChild(movieOverlay);
-  
-          movieGrid.appendChild(movieItem);
-        });
-      })
-      .catch(function(error) {
-        console.log('Error al cargar el archivo JSON:', error);
-      });
-  });
-   
+var videoPlayer = document.getElementById('videoPlayer');
+var playButton = document.getElementById('playButton');
+var pauseButton = document.getElementById('pauseButton');
+var volumeRange = document.getElementById('volumeRange');
+var fullscreenButton = document.getElementById('fullscreenButton');
+
+playButton.addEventListener('click', function() {
+  videoPlayer.play();
+  playButton.style.display = 'none';
+  pauseButton.style.display = 'block';
+});
+
+pauseButton.addEventListener('click', function() {
+  videoPlayer.pause();
+  pauseButton.style.display = 'none';
+  playButton.style.display = 'block';
+});
+
+volumeRange.addEventListener('input', function() {
+  videoPlayer.volume = volumeRange.value;
+});
+
+fullscreenButton.addEventListener('click', function() {
+  if (videoPlayer.requestFullscreen) {
+    videoPlayer.requestFullscreen();
+  } else if (videoPlayer.mozRequestFullScreen) {
+    videoPlayer.mozRequestFullScreen();
+  } else if (videoPlayer.webkitRequestFullscreen) {
+    videoPlayer.webkitRequestFullscreen();
+  } else if (videoPlayer.msRequestFullscreen) {
+    videoPlayer.msRequestFullscreen();
+  }
+});
